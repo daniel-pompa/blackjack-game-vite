6e522ee0710173516e57fd7d7c1f9b554f679376
Daniel <daniel.40.pompa@gmail.com>
2024-03-07 00:57:45 +0100

    feat: draw a card from the deck
    create a function to draw a card from the deck and add event listener for draw card button

--- src/blackjack/index.js
+++ src/blackjack/index.js
@@ -1,19 +1,48 @@
+import { calculateHandScore } from './usecases/calculate-score';
 import { createDeck } from './usecases/create-deck';
 import { dealInitialCards } from './usecases/deal-initial-cards';
+import { drawCard } from './usecases/draw-card';
+
+//* Constants
+const suits = ['C', 'D', 'H', 'S']; // Define an array of card suits
+const specials = ['A', 'J', 'Q', 'K']; // Define an array of special card values
 
 //* Global variables
 let deck = []; // Initialize an empty array to hold the deck of cards
-const suits = ['C', 'D', 'H', 'S']; // Define an array of card suits
-const specials = ['A', 'J', 'Q', 'K']; // Define an array of special card values
 let playerHand = []; // Initialize an empty array to hold player's hand
 let dealerHand = []; // Initialize an empty array to hold dealer's hand
 let playerScore = 0; // Initialize player's score to 0
 let dealerScore = 0; // Initialize dealer's score to 0
+
+//* HTML references
+const drawCardButton = document.querySelector('#draw-card-button');
 
 // Initializes the game by creating a deck, and dealing initial cards to player and dealer
 const initializeGame = () => {
   deck = createDeck(suits, specials);
   dealInitialCards(deck, playerHand, playerScore, dealerHand, dealerScore);
 };
+
+//* Event listeners
+// Event listener for draw card button
+drawCardButton.addEventListener('click', () => {
+  const card = drawCard(deck);
+  playerHand.push(card);
+  playerScore = calculateHandScore(playerHand);
+  // TODO render player hand and score in the DOM
+  console.log('Mano del jugador:', playerHand);
+  console.log('Puntuación del jugador:', playerScore);
+  // Check if the player has gone over 21 or has reached 21
+  if (playerScore > 21 || playerScore === 21) {
+    // TODO disable the buttons for actions
+    console.log('Botones deshabilitados');
+    if (playerScore > 21) {
+      console.log('El jugador se ha pasado de 21. La banca gana.');
+    } else {
+      // TODO take the dealer's turn
+      console.log('Turno de la banca');
+    }
+  }
+});
 
 initializeGame();
---
+++ src/blackjack/usecases/draw-card.js
@@ -0,0 +1,12 @@
+/**
+ * Draws a card from the deck
+ * @param {Array<String>} deck - The deck of cards
+ * @returns {string} The card drawn from the deck
+ * @throws {Error} If the deck is empty
+ */
+export const drawCard = deck => {
+  if (deck.length === 0) {
+    throw new Error('No hay cartas en la baraja');
+  }
+  return deck.pop();
+};
